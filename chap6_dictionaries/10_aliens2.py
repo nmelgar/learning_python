0a30308893498248eb60d9e53b20a86ba1e5a885
nmelgar <nmelgar16013@gmail.com>
2021-04-26 09:17:04 -0500

    26-04-09-16--changes to 10_aliens2.py adding aliens

--- chap6_dictionaries/10_aliens2.py
+++ chap6_dictionaries/10_aliens2.py
@@ -1,9 +1,25 @@
+#make an empty list for storing aliens
 aliens = []
 
 #make 30 green aliens
 for alien_number in range(30):
+    #creates the new alien individually
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    #adds the new_alien dictionary to the aliens list
+    #these actions are repeated 30 times to create the 30 aliens in the range
     aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
+
 #show the first 5 aliens
 for alien in aliens[:5]:
     print(alien)
