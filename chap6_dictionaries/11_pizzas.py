39a8e0ded5af153914c3b54d84d14cfa75e8ffed
nmelgar <nmelgar16013@gmail.com>
2021-04-27 18:33:26 -0500

    27-04-18-33-changes to fav langues and_pizzas.py

--- chap6_dictionaries/11_pizzas.py
+++ chap6_dictionaries/11_pizzas.py
@@ -1,1 +1,13 @@
-pizza = 
+pizza = {
+    'crust': 'thick',
+    #this toppings has a list as its value
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+
+#summarize the order
+print(f"You ordered a {pizza['crust']}-crust pizza "
+    #the following line needs to be indented
+    "with the following toppins:")
+
+for topping in pizza['toppings']:
+    print("\t" + topping)
--- chap6_dictionaries/8_favorite_language.py
+++ chap6_dictionaries/8_favorite_language.py
@@ -1,17 +1,21 @@
 favorite_language = {
-    'sarah': 'c-sharp',
-    'peter': 'python',
-    'hannah': 'java',
-    'mark': 'javascript',
-    'phil': 'ruby',
+    'sarah': ['c-sharp', 'ruby'],
+    'peter': ['python', 'ruby'],
+    'hannah': ['java', 'python'],
+    'mark': ['javascript','ruby'],
+    'phil': ['ruby', 'python'],
 }
 
-language = favorite_language['sarah'].title()
-print(f"Sarah's favorite laguage is {language}.")
 
-for name, language in favorite_language.items():
-    print(f"{name.title()}'s favorite language is {language.title()}.")
 
+#use the singular version without lists inside the dictionary
+#for name, language in favorite_language.items():
+#    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+for name, languages in favorite_language.items():
+    print(f"{name.title()}'s favorite language are:")
+    for language in languages:
+        print(f"\t{language.title()}")
 #Loop just through the keys using the keys() method
 print(f"\nTHe name of the users that love programming are:")
 for name in favorite_language.keys():
