bd24171fba3bb5c2635c51a349d081dc43f86fe0
nmelgar <nmelgar16013@gmail.com>
2021-12-01 10:48:48 -0600

    10:49--added templates for home and pet_details

--- django/wisdompets/adoptions/views.py
+++ django/wisdompets/adoptions/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
-
+#import pet class
 from .models import Pet
-
 
 def home(request):
     pets = Pet.objects.all()
+    #renders html to the templates
+    #render requires templates to work
+    #1st argument request object | 2 string with template name we want to use
     return render(request, 'home.html', {
+        #dictionary with data to be available in the template
+        #key
         'pets': pets,
     })
 
