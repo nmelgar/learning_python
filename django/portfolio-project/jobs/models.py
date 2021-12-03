6700091fd5db62544402e97bc99a01f9be728f1e
nmelgar <nmelgar16013@gmail.com>
2021-12-03 09:42:04 -0600

    09:41--started with the DB

--- django/portfolio-project/jobs/models.py
+++ django/portfolio-project/jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    #images
+    #property with a image field
+    images = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
