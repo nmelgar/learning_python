39d8dbf6d99673d0190dda470bdcfe38174a91c9
nmelgar <nmelgar16013@gmail.com>
2021-12-03 18:55:42 -0600

    18:55--DB set up correctly, 1st migrations done

--- django/portfolio-project/portfolio/settings.py
+++ django/portfolio-project/portfolio/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        #next line will be for where the DB will be
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
