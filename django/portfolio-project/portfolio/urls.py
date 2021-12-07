b401a420fcc35a7b57625be447d34c517f5f2407
nmelgar <nmelgar16013@gmail.com>
2021-12-07 08:32:03 -0600

    08:31--added html, static files, bootstrap

--- django/portfolio-project/portfolio/urls.py
+++ django/portfolio-project/portfolio/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #when someone types /admin, he will be taken to admin
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
