8f6130f3297206c49fe520f9159a1464d6c75415
nmelgar <nmelgar16013@gmail.com>
2023-03-03 11:47:55 -0600

    Added: -admin -login auth -notes model -migrations

--- django/essential_training/home/views.py
+++ django/essential_training/home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here
 #everytime this function receives a request
 #it will return what's in tht return
 def home(request):
-    return HttpResponse('hello world')
+    return render(request, 'home/welcome.html', {})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
--- django/essential_training/smartnotes/settings.py
+++ django/essential_training/smartnotes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # created apps
     'home',
+    'notes',
 ]
 
 MIDDLEWARE = [
