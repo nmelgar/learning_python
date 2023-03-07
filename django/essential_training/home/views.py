5f7da802198cf226abb6b89b6f1aad81d74b6c18
nmelgar <nmelgar16013@gmail.com>
2023-03-06 18:27:41 -0600

    Added: - display notes from db in webpage - class-based views -login for class-based views

--- django/essential_training/home/views.py
+++ django/essential_training/home/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here
 #everytime this function receives a request
 #it will return what's in tht return
-def home(request):
-    return render(request, 'home/welcome.html', {})
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+
+class AuthorizedView(TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
+
+ 
