7c59362fa3976af346ad59253834d37d0c72e372
nmelgar <nmelgar16013@gmail.com>
2023-03-07 20:42:33 -0600

    Added: -class to edit -class to delete -delete view

--- django/essential_training/notes/forms.py
+++ django/essential_training/notes/forms.py
@@ -16,8 +16,8 @@ class NotesForm(forms.ModelForm):
             'text': "Write your thoughts here"
         }
 
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if 'Django' not in title:
-            raise ValidationError('We only accept notes about Django')
-        return title
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     # if 'Django' not in title:
+    #     #     raise ValidationError('We only accept notes about Django')
+    #     return title
