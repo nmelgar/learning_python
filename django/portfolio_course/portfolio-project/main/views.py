2c833ce442f851277e92253b1fb8b7a83429cb22
nmelgar <nmelgar16013@gmail.com>
2023-03-24 08:38:01 -0600

    Added: -bootstrap design -display 3 projects at main page -headings -buttons working at main

--- django/portfolio_course/portfolio-project/main/views.py
+++ django/portfolio_course/portfolio-project/main/views.py
@@ -4,6 +4,5 @@ from projects.models import Project
 
 def home(request):
     jobs = Job.objects
-    projects = Project.objects
+    projects = Project.objects.all().order_by('id')[:3]
     return render(request, 'main/home.html', {'jobs': jobs, 'projects': projects})
-
