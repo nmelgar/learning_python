03918ac524157c0cd4b049ae5bbaa95eaeb9bd0f
nmelgar <nmelgar16013@gmail.com>
2023-03-25 13:01:02 -0600

    Added: -display 3 random projects at home page -work experience and education sections, but missing correct info - desing details

--- django/portfolio_course/portfolio-project/main/views.py
+++ django/portfolio_course/portfolio-project/main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from jobs.models import Job
 from projects.models import Project
 
+
 def home(request):
     jobs = Job.objects
-    projects = Project.objects.all().order_by('id')[:3]
+    # filter to display the first 3 items reference https://docs.djangoproject.com/en/dev/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    # projects = Project.objects.all().order_by('id')[:3]
+    projects = Project.objects.all().order_by('?')[:3]
     return render(request, 'main/home.html', {'jobs': jobs, 'projects': projects})
